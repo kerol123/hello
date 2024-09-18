1993322a26b76ccfac4e312ee309d0c8ec0cc594
kerol <ikerol@163.com>
2024-09-19 01:16:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <el-header>Vue Chrome Extension</el-header>
+    <el-main>
+      <el-button type="primary" @click="handleClick">Click Me!</el-button>
+      <p>{{ message }}</p>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const message = ref('Hello from Chrome Extension!');
+
+    const handleClick = () => {
+      message.value = 'Button Clicked!';
+    };
+
+    return { message, handleClick };
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        background: 'src/background.js'
+      },
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
+});
